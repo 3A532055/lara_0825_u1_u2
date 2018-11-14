4ac060fe4cf3ec54b489b160a4d5a913adcbb000
3A532055 <ylhc214044@gmail.com>
2018-11-14 11:30:11 +0800

    練習 2-3:改用 new 的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@
 
 Route::get('/', function () {
     //return view('welcome');
-    \App\Post::create([
-        'title'=>'test title',
+    //\App\Post::create([
+     //   'title'=>'test title',
 
-        'content'=>'test content',
-    ]);
+      //  'content'=>'test content',
+    //]);
+
+
+    $post =new\App\Post();
+    $post -> title='test title';
+    $post -> content='test content';
+    $post -> save();
 
 });
